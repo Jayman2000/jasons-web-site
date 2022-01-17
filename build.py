9751f3cb44f3c02e3590472deecda81bfa59c08c
jayman2000 <jayman2000@hyy4aj3wiqq1o6m5un17sq4m74i4btx8w4ypryri91nrauz9odhfis>
2022-01-17 07:11:52 -0500

    Use present tense for CC0 notice
    This doesn’t change the meaning of the notice, but it does make the
    notice more clear.

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-# SPDX-FileNotice: 🅭🄍1.0 This file was dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
+# SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
 # SPDX-FileContributor: Jason Yundt <swagfortress@gmail.com> (2022)
 from pathlib import Path
 from shutil import copytree, rmtree
