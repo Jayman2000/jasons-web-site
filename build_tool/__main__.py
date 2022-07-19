d50b9cfd01f47b5ee5c75bec52a7de50aa3fcce2
Jason Yundt <jason@jasonyundt.email>
2022-07-19 11:08:30 -0400

    Replace old email with new one

--- build_tool/__main__.py
+++ build_tool/__main__.py
@@ -1,5 +1,5 @@
 # SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
-# SPDX-FileContributor: Jason Yundt <swagfortress@gmail.com> (2022)
+# SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2022)
 from __future__ import annotations
 
 from argparse import ArgumentParser
